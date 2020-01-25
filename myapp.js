f6de7490864202db7893cdac734cc744450382b2
John-AE <johnamaka@gmail.com>
2020-01-25 18:28:14 -0500

    first commit

---
+++ myapp.js
@@ -0,0 +1,13 @@
+// Any method with element will return a single object.
+// Any method with elements will return an array.
+
+// console.log( document.getElementsByTagName('p') );
+
+// console.log( document.getElementById('hello') );
+
+// console.log( document.getElementsByClassName('pClass') );
+
+var pHello = document.getElementById("hello");
+
+pHello.innerText = "new world";
+pHello.innerText += " order11";
